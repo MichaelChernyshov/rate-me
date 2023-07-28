d24456bff9015f3d87f75b671bb2bb1f95620751
MichaelChernyshov <chernyshov.michaele@icloud.com>
2023-07-28 16:01:13 +0200

    Added Tag comp with styles, props and added to pag

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Button from '@/components/Button/Button'
 import HTag from '@/components/HTag/HTag'
 import P from '@/components/P/P'
+import Tag from '@/components/Tag/Tag'
 
 export default function Home() {
   return (
@@ -16,6 +17,18 @@ export default function Home() {
       <P size={'s'}>Hello</P>
       <P size={'m'}>Hello</P>
       <P size={'l'}>Hello</P>
+      <Tag size='m' color='red'>
+        Hello
+      </Tag>
+      <Tag size='s' color='green'>
+        Asss
+      </Tag>
+      <Tag size='s' color='ghost'>
+        DDdd
+      </Tag>
+      <Tag size='m' color='primary'>
+        DDdd
+      </Tag>
     </>
   )
 }
---
+++ components/Tag/Tag.tsx
@@ -0,0 +1,23 @@
+import { PProps } from './Tag.props'
+import styles from './Tag.module.css'
+import cn from 'classnames'
+
+const Tag = ({ children, size = 'm', color = 'ghost', href, className, ...props }: PProps) => {
+  return (
+    <div
+      className={cn(styles.tag, className, {
+        [styles.s]: size == 's',
+        [styles.m]: size == 'm',
+        [styles.ghost]: color == 'ghost',
+        [styles.red]: color == 'red',
+        [styles.gray]: color == 'gray',
+        [styles.green]: color == 'green',
+        [styles.primary]: color == 'primary',
+      })}
+      {...props}>
+      {href ? <a href={href}>{children}</a> : <>{children}</>}
+    </div>
+  )
+}
+
+export default Tag
