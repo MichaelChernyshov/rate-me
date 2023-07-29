dfca7a22e53b562c9f01f09edd42b572f4f4dbe7
MichaelChernyshov <chernyshov.michaele@icloud.com>
2023-07-29 13:01:09 +0200

    Added layout with footer

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import { FooterProps } from './Footer.props'
 import styles from './Footer.module.css'
 import cn from 'classnames'
+import { format } from 'date-fns'
 
-const Footer = ({ ...props }: FooterProps) => {
-  return <div {...props}>Footer</div>
+const Footer = ({ className, ...props }: FooterProps) => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>RateMe © {format(new Date(), 'yyyy')}</div>
+      <a href='#' target='_blank'>
+        User agreements
+      </a>
+      <a href='#' target='_blank'>
+        Privacy policy
+      </a>
+    </footer>
+  )
 }
 
 export default Footer
